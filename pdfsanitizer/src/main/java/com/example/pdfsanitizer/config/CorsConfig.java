d66f5487125bf325ca24a11e5b0541dbf043c08a
LAVISH <79981121+lavishpruthi@users.noreply.github.com>
2025-07-16 00:17:48 +0530

    Update CorsConfig.java

--- pdfsanitizer/src/main/java/com/example/pdfsanitizer/config/CorsConfig.java
+++ pdfsanitizer/src/main/java/com/example/pdfsanitizer/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000", "https://pdfsanitizer-frontend.vercel.app")
+        .allowedOrigins("http://localhost:3000", "https://pdfsanitizer-frontend-jjnj4cyl8-lavishs-projects-c8e1e592.vercel.app/")
         .allowedMethods("GET", "POST")
                 .allowedHeaders("*");
     }
